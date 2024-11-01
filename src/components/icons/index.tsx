d83653e5f1b46197e2de3d57b1096bbede48ce1c
Jackson Perry <jackperry2187@gmail.com>
2024-10-31 20:08:38 -0400

    SVG icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,13 @@
+import Calendar from './Calendar';
+import Curseforge from './Curseforge';
+import GitHub from './GitHub';
+import LinkedIn from './LinkedIn';
+import Modrinth from './Modrinth';
+
+export {
+    Calendar,
+    Curseforge,
+    GitHub,
+    LinkedIn,
+    Modrinth,
+}
