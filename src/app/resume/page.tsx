b276103f080f1399065a2db40dd3f307998b2800
Jackson Perry <jackperry2187@gmail.com>
2024-11-04 20:28:10 -0500

    Copy over resume embed

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,7 @@
+const Resume = () => {
+    return (
+        <embed src="https://drive.google.com/file/d/15W4lXLNYLexCrSNhO9fXYEtseRMSU4SR/preview?usp=sharing" height="1080px" width="60%" className="p-2"/>
+    );
+}
+
+export default Resume;
