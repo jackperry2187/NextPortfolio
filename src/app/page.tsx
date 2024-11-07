9a528be27ac1066db117d0abc56b55a2818618c1
Jackson Perry <jackperry2187@gmail.com>
2024-11-07 13:29:02 -0500

    Add timer effect to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,41 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import me from '../../public/images/profile_1.png';
 
 const HomePage = () => {
-  const [firstName, setFirstName] = useState("Jackson");
-  const [lastName, setLastName] = useState("Perry");
+  const [firstName, setFirstName] = useState("_");
+  const [lastName, setLastName] = useState("_");
+  let [timeRan, setTimeRan] = useState(0);
+  let [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    let interval: undefined | NodeJS.Timeout = undefined;
+    if(isActive) {
+      interval = setInterval(() => {
+        setTimeRan(timeRan => timeRan + 1);
+      }, 150);
+    }
+    else clearInterval(interval);
+
+    return () => clearInterval(interval);
+  }, [isActive, timeRan]);
+
+  useEffect(() => {
+    if(timeRan >= 7) setIsActive(false);
+    if(timeRan <= 7) setFirstName('Jackson'.substring(0, timeRan) + (timeRan < 7 ? '_' : ''));
+    if(timeRan <= 5) setLastName('Perry'.substring(0, timeRan) + (timeRan < 5 ? '_' : ''));
+  }, [timeRan]);
   // TODO: implement the cool typewriter kinda thing into this and also make it way better
 
   return (
     <div className="container flex flex-col items-center justify-center px-4 py-16">
       <div className="lg:flex">
         <div className="flex flex-grow flex-col items-center place-content-center p-2">
-          <h1 className="text-7xl">{firstName} {lastName}</h1>
+          <h1 className="text-7xl">
+            {firstName} {lastName}
+          </h1>
           <h4 className="text-xl text-slate-400">Mid-Level Full Stack Web Engineer and Minecraft Mod Developer</h4>
         </div>
         <div className="flex flex-grow flex-col items-center p-2">
