8410c25ccd21b5fd851122ba12c9762f110c5059
Jackson Perry <jackperry2187@gmail.com>
2024-12-01 13:30:02 -0500

    Update resume to 2-pager

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,6 @@
 const Resume = () => {
     return (
-        <embed src="https://drive.google.com/file/d/1FEEQuayFhtrhj7V-j4uZ3WslWXVhXqnj/preview?usp=sharing" height="1080px" width="60%" className="p-2"/>
+        <embed src="https://drive.google.com/file/d/1wLJiIPeqk7cDBlOH64yy6cM30guGvSwE/preview?usp=sharing" height="1080px" width="60%" className="p-2"/>
     );
 }
 
